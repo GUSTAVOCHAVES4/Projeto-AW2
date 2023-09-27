a85032b44b1f3815fa30ab0a0fef22a457c1978e
GUSTAVOCHAVES4 <gustavonoronha854@gmail.com>
2023-09-27 06:33:56 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto AW2 - Site de música/artistas</title>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <!-- bootstrap cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<!-- header section starts -->
+
+<header class="header fixed-top">
+
+    <div class="container">
+        
+        <div class="row align-items-center justify-content-between">
+
+        <a href="#home" class="logo">dental<span>Care.</span></a>
+
+        <nav class="nav">
+            <a href="#home">home</a>
+            <a href="#about">about</a>
+            <a href="#services">services</a>
+            <a href="#reviews">reviews</a>
+            <a href="#contact">contact</a>
+        </nav>
+
+        <a href="#contact" class="link-btn">make appointment</a>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+
+
+        </div>
+
+    </div>
+
+
+</header>
+
+
+
+
+
+
+
+
+<!-- header section ends -->
+
+<!-- home section starts -->
+
+<section class="home" id="home">
+ 
+    <div class="container">
+
+        <div class="row min-vh-100 align-items-center">
+            <div class="content text-center text-md-left">
+                <h3>let us brighten your smile</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                <a href="#contact" class="link-btn">make appointment</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts -->
+
+<section class="about" id="about">
+    <div class="container">
+
+        <div class="row align-items-center">
+
+            <div class="col-md-6 image">
+                <img src="images/konan.jpg" class="w-100 mb-5 mb-md-0" alt="">
+            </div>
+
+            <div class="col-md-6 content">
+                <span>about us</span>
+                <h3>True Healthcare For Your Family</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt sunt animi quia est? Fuga debitis officia iure saepe ut! Voluptates, nisi earum quis sint at inventore. Vel debitis magni suscipit.</p>
+                <a href="#contact" class="link-btn">make appointment</a>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+
+
+<!-- services section starts -->
+
+<div class="section services" id="services">
+    <h1 class="heading">Our services</h1>
+
+    <div class="box-container container">
+
+        <div class="box">
+            <img src="" alt="">
+            <h3>Alignment specialist</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci fuga dolores deleniti. Nobis dignissimos dolores eveniet obcaecati illo autem, corrupti odit expedita quod, saepe ipsam. Assumenda odit laboriosam recusandae? Natus.</p>
+        </div>
+
+        <div class="box">
+            <img src="" alt="">
+            <h3>Cosmetic dentistry</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci fuga dolores deleniti. Nobis dignissimos dolores eveniet obcaecati illo autem, corrupti odit expedita quod, saepe ipsam. Assumenda odit laboriosam recusandae? Natus.</p>
+        </div>
+
+        <div class="box">
+            <img src="" alt="">
+            <h3>Oral Hygiene Experts</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci fuga dolores deleniti. Nobis dignissimos dolores eveniet obcaecati illo autem, corrupti odit expedita quod, saepe ipsam. Assumenda odit laboriosam recusandae? Natus.</p>
+        </div>
+
+        <div class="box">
+            <img src="" alt="">
+            <h3>Root Canal Specialist</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci fuga dolores deleniti. Nobis dignissimos dolores eveniet obcaecati illo autem, corrupti odit expedita quod, saepe ipsam. Assumenda odit laboriosam recusandae? Natus.</p>
+        </div>
+
+        <div class="box">
+            <img src="" alt="">
+            <h3>Live Dental Advisory</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci fuga dolores deleniti. Nobis dignissimos dolores eveniet obcaecati illo autem, corrupti odit expedita quod, saepe ipsam. Assumenda odit laboriosam recusandae? Natus.</p>
+        </div>
+
+        <div class="box">
+            <img src="" alt="">
+            <h3>Cravity Inspection</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci fuga dolores deleniti. Nobis dignissimos dolores eveniet obcaecati illo autem, corrupti odit expedita quod, saepe ipsam. Assumenda odit laboriosam recusandae? Natus.</p>
+        </div>
+
+    </div>
+
+</div>
+
+<!-- services section ends -->
+
+
+<!-- process section starts -->
+
+<section class="process">
+
+    <h1 class="heading">work process</h1>
+
+    <div class="box-container container">
+        <div class="box">
+            <img src="" alt="">
+            <h3>Cosmetic dentistry</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, pariatur culpa illo explicabo iure tempore maiores eaque facere soluta architecto harum veritatis, omnis libero impedit molestias atque? Optio, ducimus maiores?</p>
+        </div>
+
+        <div class="box">
+            <img src="" alt="">
+            <h3>Pediatric dentistry</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, pariatur culpa illo explicabo iure tempore maiores eaque facere soluta architecto harum veritatis, omnis libero impedit molestias atque? Optio, ducimus maiores?</p>
+        </div>
+
+        <div class="box">
+            <img src="" alt="">
+            <h3>Dental Implants</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, pariatur culpa illo explicabo iure tempore maiores eaque facere soluta architecto harum veritatis, omnis libero impedit molestias atque? Optio, ducimus maiores?</p>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- process section ends -->
+
+<!-- reviews section starts -->
+
+<section class="reviews" id="reviews">
+
+    <h1 class="heading">satisfied clients</h1>
+
+    <div class="box-cotainer container">
+
+        <div class="box">
+            <img src="" alt="">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum repudiandae aliquam quasi error atque ducimus eveniet sapiente? Labore perspiciatis, quia reprehenderit earum qui repudiandae veritatis fugit fuga optio illo vero?</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>john deo</h3>
+            <span>satisfied client</span>
+        </div>
+
+        <div class="box">
+            <img src="" alt="">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum repudiandae aliquam quasi error atque ducimus eveniet sapiente? Labore perspiciatis, quia reprehenderit earum qui repudiandae veritatis fugit fuga optio illo vero?</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>john deo</h3>
+            <span>satisfied client</span>
+        </div>
+
+        <div class="box">
+            <img src="" alt="">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum repudiandae aliquam quasi error atque ducimus eveniet sapiente? Labore perspiciatis, quia reprehenderit earum qui repudiandae veritatis fugit fuga optio illo vero?</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>john deo</h3>
+            <span>satisfied client</span>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+<!-- contact section starts -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- contact section ends -->
+
+
+
+
+
+
+
+
+
+<!-- custom js file link -->
+<script src="js/script.js"></script>
+    
+</body>
+</html>
